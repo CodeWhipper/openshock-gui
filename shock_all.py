8305651aee66da40fb4960540394c2ca23a0449e
isalmailra <isalmailra@isalmailra.isalmailra>
2025-10-30 15:12:37 -0500

    adding shock all & renaming

---
+++ shock_all.py
@@ -0,0 +1,34 @@
+import requests
+import numpy as np
+import json
+import random
+
+device='019a361e-1e0d-737a-ba70-4ce844330c42'
+shocker=[]
+
+dev = requests.get(
+    f"https://api.openshock.app/1/devices/{device}/shockers",
+    headers={
+      "OpenShockToken": "kUH9pi6cmRjke4ADvcOoQOyY9tURe0ObNw6hZMlHnTc6PJi8mCd1Gae3tGnH1ESy"
+    }
+)
+
+for i in json.loads(dev.content)["data"]:
+  requests.post(
+      "https://api.openshock.app/1/shockers/control",
+      headers={
+        "Content-Type": "application/json",
+        "OpenShockToken": "kUH9pi6cmRjke4ADvcOoQOyY9tURe0ObNw6hZMlHnTc6PJi8mCd1Gae3tGnH1ESy"
+      },
+      json=[
+        {
+          "id": i["id"],
+          "type": "vibrate",
+          "intensity": 5,
+          "duration": 300,
+          "exclusive": True
+        }
+      ]
+  )    
+
+                                                                                                                                                                                                                                        
