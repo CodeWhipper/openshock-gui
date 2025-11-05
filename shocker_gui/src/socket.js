92047a993c3c55099e0cc5a7c43523c95023f5ac
Florian Hösler <florian.hoesler@gmail.com>
2025-11-06 00:15:11 +0100

    fixes last issues

--- shocker_gui/src/socket.js
+++ shocker_gui/src/socket.js
@@ -1,8 +1,10 @@
 import { io } from "socket.io-client";
-const host = import.meta.env.VITE_HOST || "node-backend";
+const host =  import.meta.env.VITE_HOST || "localhost";
 const port = import.meta.env.VITE_PORT || "4000";
+const production = import.meta.env.VITE_PRODUKTION === "true"
 
-export const socket = io(window.location.origin, {
+
+export const socket = io( production ? window.location.origin : `http://${host}:${port}`, {
   path: '/socket.io',
   transports: ["websocket"],
 });
